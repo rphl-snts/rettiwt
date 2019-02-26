edfdd32c121e2673aff6d99234376a8ead2aa2b9
Raphael Santos <raphaelsantos.j@gmail.com>
2019-02-26 08:39:13 -0300

    home page

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import twitterLogo from '../twitter.svg';
+import './Login.css';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="login-wrapper ">
+        <img src={twitterLogo} alt="twitterLogo" />
+        <form>
+          <input placeholder="Nome de usuário"/>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ frontend/src/pages/Timeline.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import './Timeline';
+
+export default class TimeLine extends Component {
+  render() {
+    return <h1>TimeLine</h1>;
+  }
+}
